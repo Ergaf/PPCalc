7df6d9c4b9dd92404587ed690b47fe7126990598
Ergaf <Bymbarawko@gmail.com>
2022-09-12 01:36:45 +0300

    add rend settings

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ class file {
     container;
     type;
     format;
+    sides;
+    color;
     constructor (name) {
         this._name = name;
         this._id = new Date();
@@ -12,7 +14,9 @@ class file {
         let Item = document.createElement('div');
         this.container = Item;
 
-        Item.classList.add('oneFileInFilesContainer');
+        Item.classList.add('card');
+        Item.classList.add('bg-light');
+        Item.style.cssText = "display: inline-block; padding: 1vmin;"
         filesAllContainer.appendChild(Item);
         Item.onmousedown = this.pick.bind( this);
 
@@ -34,10 +38,20 @@ class file {
                 e.removePick()
             }
         })
-        this.container.classList.add("oneFileInFilesContainerPicked")
+        this.container.classList.remove("bg-light")
+        this.container.classList.add("bg-dark")
+        this.container.classList.add("text-white")
+
+        let presetName = document.querySelector(".presetName")
+        presetName.innerText = this.type
+
+        thisFile = this._id
+        this.renderSettings()
     }
     removePick() {
-        this.container.classList.remove("oneFileInFilesContainerPicked")
+        this.container.classList.remove("bg-dark")
+        this.container.classList.remove("text-white")
+        this.container.classList.add("bg-light")
     }
     deleteThis() {
         allFiles.forEach(e => {
@@ -47,13 +61,24 @@ class file {
             }
         })
     }
+
+    renderSettings() {
+        formatSelect.value = this.format
+        sidesSelect.value = this.sides
+        colorSelect.value = this.color
+    }
+
 }
 const allFiles = []
+let thisFile;
 
-const filesContainer = document.querySelector(".allFilesContainer");
-const addFileButton = document.querySelector(".addFileButton");
+const addFileButton = document.querySelector("#addFileButton");
 addFileButton.addEventListener("click", function () {
     let file1 = new file("file")
     allFiles.push(file1)
     file1.createVisitContainer()
 })
+
+let formatSelect = document.querySelector("#formatSelect")
+let sidesSelect = document.querySelector("#sidesSelect")
+let colorSelect = document.querySelector("#colorSelect")
