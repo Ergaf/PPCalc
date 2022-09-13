a18a17b05668fd6e9269bbd296bbe0f66d641ab0
Ergaf <Bymbarawko@gmail.com>
2022-09-13 12:09:35 +0300

    addProblem

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 class file {
     container;
+    nameContainer;
     type;
     format;
     sides;
@@ -29,6 +30,7 @@ class file {
         let nameContainer = document.createElement('div');
         nameContainer.innerHTML = this._name;
         nameContainer.classList.add('.inlineBlock');
+        this.nameContainer = nameContainer;
         Item.appendChild(nameContainer);
 
         let cancelButton = document.createElement('div');
@@ -38,21 +40,24 @@ class file {
         Item.appendChild(cancelButton);
     }
 
-    pick(){
-        allFiles.forEach(e => {
-            if(e._id !== this._id){
-                e.removePick()
-            }
-        })
-        this.container.classList.remove("bg-light")
-        this.container.classList.add("bg-dark")
-        this.container.classList.add("text-white")
+    pick(e){
+        if(e.target === this.container || e.target === this.nameContainer){
+            allFiles.forEach(e => {
+                if(e._id !== this._id){
+                    e.removePick()
+                }
+            })
 
-        let presetName = document.querySelector(".presetName")
-        presetName.innerText = this.type
+            this.container.classList.remove("bg-light")
+            this.container.classList.add("bg-dark")
+            this.container.classList.add("text-white")
 
-        thisFile = this._id
-        this.renderSettings()
+            let presetName = document.querySelector(".presetName")
+            presetName.innerText = this.type
+
+            thisFile = this
+            this.renderSettings()
+        }
     }
     removePick() {
         this.container.classList.remove("bg-dark")
@@ -74,8 +79,9 @@ class file {
         colorSelect.value = this.color
     }
 
-    createSelect() {
+    createSelect(selectType) {
         let select = document.createElement("select")
+
     }
 
 }
@@ -92,11 +98,9 @@ addFileButton.addEventListener("click", function () {
 let formatSelect = document.querySelector("#formatSelect")
 let sidesSelect = document.querySelector("#sidesSelect")
 let colorSelect = document.querySelector("#colorSelect")
-
 let destinySelect = document.querySelector("#DestinySelect")
 
 let prices;
-console.log("lol");
 fetch('https://script.googleusercontent.com/macros/echo?user_content_key=wLSQSatR6bZv9i8U5VtiOsa7GMSDGnnZijrnGFZE1_jwd1QJkdBz8Sl8ITa_TvVjVpf_ByOh6IcFuOZ7evsUSo_9NYtdFJYTm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnDbwAl7CMxVAiYx-XcQGm2-pK98VFRlg2L1Bgi9-N5lGP8ipd0KGqDVV0UksueULwVpami56uyJ4IxkRYgJm5B_wls8-MAHEtdz9Jw9Md8uu&lib=MKqsPpMpIdvM_NE9JC918gzq7P1CHZY8E')
     .then(response => response.json())
     .then(json => {
@@ -122,4 +126,59 @@ fetch('https://script.googleusercontent.com/macros/echo?user_content_key=wLSQSat
         })
         console.log(data)
         prices = data
+        document.querySelector(".mainContainer").classList.remove("nonDisplay")
+        document.querySelector(".download").classList.add("nonDisplay")
     })
+
+
+document.querySelector("#document").addEventListener("click", function () {
+    document.querySelector("#formatSelect").value = "A4"
+    document.querySelector("#sidesSelect").value = "Односторонній"
+    document.querySelector("#colorSelect").value = "Чорно-білий"
+    thisFile.format = "A4"
+})
+document.querySelector("#presentation").addEventListener("click", function () {
+    document.querySelector("#formatSelect").value = "A4"
+    document.querySelector("#sidesSelect").value = "Односторонній"
+    document.querySelector("#colorSelect").value = "Кольоровий"
+})
+document.querySelector("#poster").addEventListener("click", function () {
+    document.querySelector("#formatSelect").value = "A3"
+    document.querySelector("#sidesSelect").value = "Односторонній"
+    document.querySelector("#colorSelect").value = "Кольоровий"
+})
+document.querySelector("#card").addEventListener("click", function () {
+    document.querySelector("#formatSelect").value = "A6"
+    document.querySelector("#sidesSelect").value = "Односторонній"
+    document.querySelector("#colorSelect").value = "Кольоровий"
+})
+document.querySelector("#visitCard").addEventListener("click", function () {
+    document.querySelector("#formatSelect").value = "50x90"
+    document.querySelector("#sidesSelect").value = "Односторонній"
+    document.querySelector("#colorSelect").value = "Кольоровий"
+})
+document.querySelector("#sticker").addEventListener("click", function () {
+    document.querySelector("#formatSelect").value = "A6"
+    document.querySelector("#sidesSelect").value = "Односторонній"
+    document.querySelector("#colorSelect").value = "Кольоровий"
+})
+document.querySelector("#tags").addEventListener("click", function () {
+    document.querySelector("#formatSelect").value = "A6"
+    document.querySelector("#sidesSelect").value = "Односторонній"
+    document.querySelector("#colorSelect").value = "Кольоровий"
+})
+document.querySelector("#brochure").addEventListener("click", function () {
+    document.querySelector("#formatSelect").value = "A4"
+    document.querySelector("#sidesSelect").value = "Двосторонній"
+    document.querySelector("#colorSelect").value = "Кольоровий"
+})
+document.querySelector("#note").addEventListener("click", function () {
+    document.querySelector("#formatSelect").value = "A5"
+    document.querySelector("#sidesSelect").value = "Односторонній"
+    document.querySelector("#colorSelect").value = "Кольоровий"
+})
+document.querySelector("#calendar").addEventListener("click", function () {
+    document.querySelector("#formatSelect").value = "A6"
+    document.querySelector("#sidesSelect").value = "Односторонній"
+    document.querySelector("#colorSelect").value = "Кольоровий"
+})
